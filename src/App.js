9e64f6b79c9430a79d3dd57113ffce04dfc88186
Arnab-31 <57807670+Arnab-31@users.noreply.github.com>
2021-10-03 03:59:05 +0530

    Map, markings, and markers added

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Map from './Components/Map';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-container">
+        <Map />
+      </div>
+     
+    </div>
+  );
+}
+
+export default App;
